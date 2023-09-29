532522bb2077be0c736fa9a8cc1e311e654c24ab
Raja450 <69240883+Raja450@users.noreply.github.com>
2023-09-29 16:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 //your JS code here. If required.
- document.getElementById('submit').addEventListener('click', function() {
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const confirmPassword = document.getElementById('confirm-password').value;
+const btn=document.getElementById('submit');
+const name=document.getElementById('name');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const Cpassword=document.getElementById('confirm-password');
+console.log("here"+ password.value +" "+Cpassword.value);
 
-      if (password === confirmPassword) {
-        const userData = {
-          name: name,
-          email: email,
-          password: password
-        };
+btn.addEventListener("click",function(){
+	if(password.value !== Cpassword.value){
+		alert('Passwords do not match');
+	}else{
+		sessionStorage.setItem('name',name);
+		sessionStorage.setItem('email',email);
+		sessionStorage.setItem('password',password);
 
-        // Store data in session storage
-        sessionStorage.setItem('user', JSON.stringify(userData));
-
-        // Show success message
-        alert('Sign up successful!');
-      } else {
-        // Show error message
-        alert('Passwords do not match');
-      }
-    });
-  
+		alert('Sign up successful!');
+	}
+})
